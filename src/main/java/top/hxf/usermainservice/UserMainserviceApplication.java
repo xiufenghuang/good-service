2eb82d0ae8da2a3a49ff8d85386672a5cc5ccbed
hxf13 <2994198039@qq.com>
2024-09-04 13:38:27 +0800

    first commit

---
+++ src/main/java/top/hxf/usermainservice/UserMainserviceApplication.java
@@ -0,0 +1,13 @@
+package top.hxf.usermainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMainserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMainserviceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/top/hxf/usermainservice/controller/UserControllert.java
@@ -0,0 +1,7 @@
+/**
+* 功能：
+* 作者：程序员青戈
+* 日期：2024/9/4 9:26
+*/
+public class UserControllert {
+}
